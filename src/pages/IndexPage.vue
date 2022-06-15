4c54a002dc50a743d5036e56361ef9c346e9a5ac
Tut-k0 <Tut_k0@protonmail.com>
2022-06-14 19:25:38 -0500

    Frontend initial commit.
    * Initialized Quasar project.
    
    * Made the home page.
    
    * Added dark mode toggle to toolbar avatar.
    
    * Configured Quasar for PWA mode.

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="column">
+      <q-btn color="primary" label="HOST" size="xl" />
+      <div class="flex-break q-py-md"></div>
+      <div class="text-center" style="font-size: xxx-large">OR</div>
+      <div class="flex-break q-py-md"></div>
+      <q-btn color="secondary" label="JOIN" size="xl" />
+      <div class="flex-break q-py-lg"></div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
