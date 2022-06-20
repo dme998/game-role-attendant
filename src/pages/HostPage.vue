f83512a32f8375bb38f34c8972b3d9184575965a
Tut-k0 <Tut_k0@protonmail.com>
2022-06-19 22:03:38 -0500

    Prettify HostPage.

--- src/pages/HostPage.vue
+++ src/pages/HostPage.vue
@@ -1,18 +1,15 @@
 <template>
   <q-page class="flex flex-center">
     <div class="q-pa-md">
-
-      <q-form
-        @submit="onSubmit"
-        @reset="onReset"
-        class="q-gutter-md"
-      >
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
         <q-input
           filled
           v-model="name"
           label="User Name"
           lazy-rules
-          :rules="[ val => val && val.length > 0 || 'Please enter a username!']"
+          :rules="[
+            (val) => (val && val.length > 0) || 'Please enter a username!',
+          ]"
         />
 
         <q-select
@@ -30,76 +27,83 @@
           label="Number of players"
           lazy-rules
           :rules="[
-          val => val !== null && val !== '' || 'Please select amount of players',
-          val => val >= 5 && val <= 10 || `Please select within ${minPlayer} ➡️ ${maxPlayer} players`
-        ]"
+            (val) =>
+              (val !== null && val !== '') || 'Please select amount of players',
+            (val) =>
+              (val >= 5 && val <= 10) ||
+              `Please select within ${minPlayer} ➡️ ${maxPlayer} players`,
+          ]"
         />
 
         <div>
-          <q-btn label="Submit" type="submit" color="primary"/>
-          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn
+            label="Reset"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
         </div>
       </q-form>
-
     </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { ref } from 'vue';
+import { ref } from "vue";
 import { useQuasar } from "quasar";
 
-const stringOptions = ['Secret Hitler',]
-const minPlayer = 5
-const maxPlayer = 10
+const stringOptions = ["Secret Hitler"];
+const minPlayer = 5;
+const maxPlayer = 10;
 
 export default defineComponent({
   name: "HostPage",
 
-  setup () {
-    const $q = useQuasar()
+  setup() {
+    const $q = useQuasar();
 
-    const name = ref(null)
-    const playerNum = ref(null)
-    const accept = ref(false)
-    const options = ref(stringOptions)
+    const name = ref(null);
+    const playerNum = ref(null);
+    const accept = ref(false);
+    const options = ref(stringOptions);
 
     return {
       name,
       playerNum,
       accept,
-      model: ref('Secret Hitler'),
+      model: ref("Secret Hitler"),
       stringOptions,
       options,
       minPlayer,
       maxPlayer,
 
-      onSubmit () {
+      onSubmit() {
         if (accept.value !== true) {
           $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'You need to accept the license and terms first'
-          })
-        }
-        else {
+            color: "red-5",
+            textColor: "white",
+            icon: "warning",
+            message: "You need to accept the license and terms first",
+          });
+        } else {
           $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'cloud_done',
-            message: 'Submitted'
-          })
+            color: "green-4",
+            textColor: "white",
+            icon: "cloud_done",
+            message: "Submitted",
+          });
         }
       },
 
-      onReset () {
-        name.value = null
-        playerNum.value = null
-        accept.value = false
-      }
-    }
-  }
+      onReset() {
+        name.value = null;
+        playerNum.value = null;
+        accept.value = false;
+      },
+    };
+  },
 });
 </script>
