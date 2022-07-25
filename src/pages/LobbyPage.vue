fb48286be9b46d9787de7b7595cdd27590e9d6e6
Tut-k0 <Tut_k0@protonmail.com>
2022-07-25 17:30:17 -0500

    Update LobbyPage to work a bit better.

--- src/pages/LobbyPage.vue
+++ src/pages/LobbyPage.vue
@@ -11,98 +11,43 @@
           </q-item>
           <q-item-label header>Player List</q-item-label>
 
-          <q-separator spaced inset="item" />
-
-          <q-item>
-            <q-item-section top avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="User Avatar"/>
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Tut_k0</q-item-label>
-            </q-item-section>
-
-            <q-item-section side>
-              <q-icon name="star" color="yellow" />
-            </q-item-section>
-          </q-item>
-
-          <q-separator spaced inset="item" />
-
-          <q-item>
-            <q-item-section top avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="User Avatar"/>
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>dme_998</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator spaced inset="item" />
-
-          <q-item>
-            <q-item-section top avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="User Avatar"/>
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Drizz</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator spaced inset="item" />
-
-          <q-item>
-            <q-item-section top avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="User Avatar"/>
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Finne</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator spaced inset="item" />
-
-          <q-item>
-            <q-item-section top avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="User Avatar"/>
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Scav From Tarkov</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator spaced inset="item" />
-
-          <q-item>
-            <q-item-section top avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="User Avatar"/>
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Turtle</q-item-label>
-            </q-item-section>
-          </q-item>
-
           <q-separator spaced />
+
+          <ul style="list-style: none; padding-left: 0">
+            <li v-for="player in players" :key="player.id">
+              <q-item>
+                <q-item-section top avatar>
+                  <q-avatar>
+                    <img
+                      src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      alt="User Avatar"
+                    />
+                  </q-avatar>
+                </q-item-section>
+
+                <q-item-section>
+                  <q-item-label>{{ player.userName }}</q-item-label>
+                </q-item-section>
+
+                <div v-if="player.isHost">
+                  <q-item-section side>
+                    <q-icon name="star" color="yellow" />
+                  </q-item-section>
+                </div>
+              </q-item>
+
+              <q-separator spaced inset="item" />
+            </li>
+          </ul>
+
           <div style="text-align: center; padding-top: 3px">
             <q-btn label="Submit" type="submit" color="primary"></q-btn>
-            <q-btn label="Leave" type="button" color="negative"></q-btn>
+            <q-btn
+              @click="onAbort"
+              label="Leave"
+              type="button"
+              color="negative"
+            ></q-btn>
           </div>
         </q-list>
       </q-form>
@@ -131,11 +76,48 @@ export default {
 
       onAbort() {
         $q.notify({
-          color: "red-2",
+          color: "red-5",
           textColor: "white",
-          icon: "cloud_done",
+          type: "negative",
           message: "Aborted!",
         });
+      },
+    };
+  },
+
+  data() {
+    return {
+      players: {
+        player1: {
+          id: 0,
+          userName: "Tut_k0",
+          isHost: true,
+        },
+        player2: {
+          id: 1,
+          userName: "dme_998",
+          isHost: false,
+        },
+        player3: {
+          id: 2,
+          userName: "Drizz",
+          isHost: false,
+        },
+        player4: {
+          id: 3,
+          userName: "Finne",
+          isHost: false,
+        },
+        player5: {
+          id: 4,
+          userName: "Scav From Tarkov",
+          isHost: false,
+        },
+        player6: {
+          id: 5,
+          userName: "Turtle",
+          isHost: false,
+        },
       },
     };
   },
