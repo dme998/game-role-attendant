8fd931b70efcb25e29afd1c12b25047c47b98a54
Tut-k0 <Tut_k0@protonmail.com>
2022-07-25 21:20:10 -0500

    Backend init commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
