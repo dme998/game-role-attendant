9a9965da78835724fd9bcdde79e7c6ead064b194
dme998 <dme998@umsystem.edu>
2022-12-27 21:08:08 -0600

    Update rulesets.js
    Added logic to pushing messages in rulesets for player roles

--- server/rulesets.js
+++ server/rulesets.js
@@ -50,27 +50,65 @@ class SecretHitler {
     }
 
     setRolesForPlayers() {
-        // Copy players to push and pop freely.
+        
+		// Copy players to push and pop freely.
         let players = self.players;
         const playerCount = self.players.length;
         const rules = this.determineRoleCount(playerCount);
         let playerRoles = []
-        for (let i = 0; i < rules.liberalCount; i++) {
-            const playerIndex = choice(players)
-            playerRoles.push({[players[playerIndex].entityId]: "Liberal", color: "info", message: `${players[playerIndex].userName}, you are a Liberal! Enact Liberal policies and try to assassinate Hitler.`})
-            players = removeItem(players, players[playerIndex])
+		
+		// shuffle the working array
+		let workingElement;			
+		for(let i = 0; i < players.length; i++) {
+			let rand = choice(players)
+			workingElement = players[i];
+			players[i] = players[rand];
+			players[rand] = workingElement;
+		}
+		
+		for(let i = 1; i < rules.fascistCount; i++) {
+			playerRoles.push({[players[i].entityID]: "Facist", color: "negative", 
+							 message: `${players[i].userName}, your Secret Role is Facist. The identity of Hitler is ${players[0].userName}. `})
+		}
+		
+		for(let i = 1 + rules.fascistCount; i < rules.liberalCount; i++) {
+			playerRoles.push({[players[i].entityID]: "Liberal", color: "info", 
+							 message: `${players[0].userName}, your Secret Role is Liberal.` })
+		}
+		
+		if (rules.fascistsKnownToHitler) {
+        	playerRoles.push({[players[0].entityID]: "Hitler", color: "maroon", 
+							  message: `${players[0].userName}, your Secret Role is Hitler and your fellow Facist is ${players[1].userName}` })
+			for(let i = 0; i < players.length; i++) {
+				
+			}
         }
-        for (let i = 0; i < rules.fascistCount; i++) {
-            const playerIndex = choice(players)
-            playerRoles.push({[players[playerIndex].entityId]: "Fascist", color: "negative", message: `${players[playerIndex].userName}, you are a Fascist! Enact Fascist policies and try to elect Hitler.`})
-            players = removeItem(players, players[playerIndex])
-        }
-        if (rules.fascistsKnownToHitler) {
-            // do something.
-        }
-        playerRoles.push({[players[0]]: "Hitler", color: "maroon"})
+		else {
+			playerRoles.push({[players[0].entityID]: "Hitler", color: "maroon",
+							 message: `${players[0].userName}, your Secret Role is Hitler. Your fellow Facists: ` })
+		}
+		
+		
+		// Update the message variable for each facist so they know who their teammates are
+		let facists = players.slice(1, 1 + rules.fascistCount)
+		for(let i = 0; i < facists.length; i++) {
+			for(let j = 0; j < facists.length; j++) {
+				let k = j + 1;
+				if (k > facists.length) k = 0;
 
-        return playerRoles
+				// i+1 is to reassociate facists element number with original players element number
+				if (j === facists.length) {
+					playerRoles[i+1].message += facists[k].userName + "."	
+				}
+				else {
+					playerRoles[i+1].message += facists[k].userName + ", "
+				}
+			}
+		}
+		
+        
+        return playerRoles 
+		
     }
 
 }
