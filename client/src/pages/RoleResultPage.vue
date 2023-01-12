293bd86c0fa6e5de3ad97cea9aff193b46f5247a
Tut-k0 <Tut_k0@protonmail.com>
2023-01-12 12:43:02 -0600

    New page for displaying role info (WIP).

---
+++ client/src/pages/RoleResultPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+console.log(
+    localStorage.getItem("role"),
+    localStorage.getItem("color"),
+    localStorage.getItem("roleMessage")
+);
+export default {
+  name: "RoleResultPage",
+}
+</script>
